2ccebb4cab1cf2425525c8efcf3fb152569d646e
Thai Nguyen <nguyenhoathai.nht@gmail.com>
2026-03-10 18:03:21 -0700

    main.jsx will be used to test the dashboard interface, use 'npm run dev'

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import App from './App.jsx'
-
+import Navbar from './components/Navbar.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div>
+      <Navbar
+        deviceName="Raspberry Pi 5"
+        status="offline"         // Set to offline to see the offline icon
+        lastUpdated="10:42 AM"
+      />
+    </div>
   </StrictMode>,
 )
