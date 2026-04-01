d5a50df21dc1ea943a874d75074f7a7e25e9135d
Thai Nguyen <nguyenhoathai.nht@gmail.com>
2026-03-31 18:59:07 -0700

    Add SensorChart component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import './styles/App.css'
 
 import Navbar from './components/Navbar.jsx'
 import SensorCard from './components/SensorCard.jsx'
+import SensorChart from './components/SensorChart.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -23,7 +24,7 @@ function App() {
           <SensorCard
             title="Temperature"
             value={30}
-            unit={" C"}
+            unit={" °C"}
             min={24}
             max={33}
           />
@@ -43,7 +44,27 @@ function App() {
             min="5.5"
             max="7.0"
           />
+        </section>
 
+        {/*SENSOR CHART*/}
+        <section className="sensor-chart-grid" style={{ marginTop: "40px" }}>
+          <SensorChart
+            title="Temperature Chart"
+            unit="°C"
+            data={[21, 24, 25, 26, 28, 27, 29, 30, 28, 27]}
+          />
+
+          <SensorChart
+            title="Humidity Chart"
+            unit="%"
+            data={[33, 55, 58, 60, 57, 62, 65, 61, 59, 63]}
+          />
+
+          <SensorChart
+            title="pH Chart"
+            unit=""
+            data={[4.0, 6.1, 6.3, 6.5, 6.4, 6.6, 6.7, 6.5, 6.8, 6.9]}
+          />
         </section>
 
       </main>
