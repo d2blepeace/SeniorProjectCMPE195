bb9854e758623c3287b9c1dd95bbfcf902f4b6ab
Vy Tran <you@example.com>
2026-04-29 20:17:20 -0700

    Remove old storage directory

--- backend/src/storage/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Database storage module"""
-
-from .db_manager import DatabaseManager
-
-# Create singleton instance
-db_manager = DatabaseManager()
-
-__all__ = ["db_manager", "DatabaseManager"]
